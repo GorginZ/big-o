637fa4b6bd84e0e9f3a3ebc2bb61beeb79ddb9b2
GorginZ <georgia.z.leng@gmail.com>
2021-01-16 13:22:02 +1100

    adding more big o notes

--- src/components/BigO.jsx
+++ src/components/BigO.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { BubbleSort } from "./BubbleSort";
 import { MergeSort } from "./MergeSort";
+import { Link } from "react-router-dom";
+
 
 export class BigO extends Component {
   static displayName = BigO.name;
@@ -12,19 +14,28 @@ export class BigO extends Component {
         <p>
           Big O notation (the order of a function) describes the complexity of a
           function or algorythm. The order of a function indicates its
-          characteristic rate of groth. The notation comes from German
-          theoretician Edmund Landau's work and is utilised in mathematics, and computer science studies.
+          characteristic rate of growth.
           <br></br>
           <br></br>
           Different algorythms may share the same complexity and be described
           with the same notation.
+         <br></br> 
+          two concret examples here
 
         </p>
+        <p>For  example,  when  analyzing  some  algorithm,  one  might  find  that  the  time  (or  the number of steps) it takes to complete a problem of size n is given by T(n) = 4 n2 - 2 n + 2. If  we  ignore  constants  (which  makes  sense  because  those  depend  on  the  particular hardware the program is run on) and slower growing terms, we could say "T(n) grows at the order of n2" and write:T(n) = O(n2).
+          *(1)  </p>
         <p>
-          further reading and references:
+          footnotes:
           <br></br>
-          https://web.mit.edu/16.070/www/lecture/big_o.pdf
+          1) author unavailable, lecture notes, Massachusetts Institute Technology available at: <a href="https://web.mit.edu/16.070/www/lecture/big_o.pdf">here</a>
+
+
+
+           <br></br>
 https://www.geeksforgeeks.org/comparison-among-bubble-sort-selection-sort-and-insertion-sort/
+
+{/* <Link to="https://web.mit.edu/16.070/www/lecture/big_o.pdf" target="_blank" onClick={(event) => {event.preventDefault(); window.open(this.makeHref("https://web.mit.edu/16.070/www/lecture/big_o.pdf"));}} /> */}
 
         </p>
       </div>
