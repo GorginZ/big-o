8ddef20a0af9f2abdd207386ce51af89efe5e3c7
GorginZ <georgia.z.leng@gmail.com>
2021-05-31 16:14:51 +1000

    working

--- curl-script.sh
+++ curl-script.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 die() { echo “${1:-something went wrong}“; exit “${2:-1}“; }
 
-# hash jq || die "jq not found"
 hash curl || die "curl not found"
 
-
-# endpoint='http://localhost/' 
 endpoint='http://server/' 
-
-# endpoint='http://server_1/' 
-
 
 ##happy path
 curl $endpoint || die "could not get root" \
