df4115d55affd9780f69833c42996cb44d10f9af
GorginZ <georgia.z.leng@gmail.com>
2021-05-31 14:07:42 +1000

    networking NQR, can run script externaly ( like on host ) with container up, but not working in compose. WIP

--- curl-script.sh
+++ curl-script.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 die() { echo “${1:-something went wrong}“; exit “${2:-1}“; }
 
-hash jq || die "jq not found"
+# hash jq || die "jq not found"
+hash curl || die "curl not found"
+
 
 endpoint='http://localhost/' 
 
 ##happy path
 curl $endpoint || die "could not get root" \
+
+
 
