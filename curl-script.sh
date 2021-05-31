f3ecd180d80fbdea19ab7734697f78c127cc0520
GorginZ <georgia.z.leng@gmail.com>
2021-05-31 11:42:49 +1000

    playing with compose to run a script /test

---
+++ curl-script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+die() { echo “${1:-something went wrong}“; exit “${2:-1}“; }
+
+hash jq || die "jq not found"
+
+endpoint='http://localhost/' 
+
+##happy path
+curl $endpoint || die "could not get root" \
+
