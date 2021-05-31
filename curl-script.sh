235e3d48cecb6effb15add4b79c180a1d06b60e5
GorginZ <georgia.z.leng@gmail.com>
2021-05-31 16:22:48 +1000

    sub hostname in docker-compose

--- curl-script.sh
+++ curl-script.sh
@@ -3,7 +3,7 @@ die() { echo “${1:-something went wrong}“; exit “${2:-1}“; }
 
 hash curl || die "curl not found"
 
-endpoint='http://server/' 
+endpoint=$APP_HOSTNAME
 
 ##happy path
 curl $endpoint || die "could not get root" \
