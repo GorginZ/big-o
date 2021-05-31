e063df6f1bc295e42be70acaa5647b79de7dd070
GorginZ <georgia.z.leng@gmail.com>
2021-05-31 16:10:37 +1000

    working

--- curl-script.sh
+++ curl-script.sh
@@ -5,7 +5,11 @@ die() { echo “${1:-something went wrong}“; exit “${2:-1}“; }
 hash curl || die "curl not found"
 
 
-endpoint='http://localhost/' 
+# endpoint='http://localhost/' 
+endpoint='http://server/' 
+
+# endpoint='http://server_1/' 
+
 
 ##happy path
 curl $endpoint || die "could not get root" \
